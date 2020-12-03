7eb81471aaa1f598b8bb1cc2ce71f0905b22f931
roostaiematwit <roostaiem@wit.edu>
2020-12-03 17:05:34 -0500

    Beginning to add connectFour into WebServer
    -began to implement connectFour code into server
    -small change to placeCommand
    -improved functionality for placeCommand condition

--- src/WebServer.java
+++ src/WebServer.java
@@ -8,10 +8,14 @@ class WebServer {
     // PLAYERS - people who will be playing game
     private static Map<String, DataOutputStream> players = new HashMap(); // p
 
+    private static boolean redWent = false;
+    
     // SPECTATORS - same as clients minus the players;
     private static Map<String, DataOutputStream> spectators = new HashMap(); // p
 
     private static int playerCount;
+    
+    private static Map<String, Character> playersArr = new HashMap();
     
     public static boolean full = false; //p
 
@@ -35,7 +39,7 @@ class WebServer {
             DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
             
             playerCount = 0;
-
+            ConnectFour board = new ConnectFour(7,6,'R','Y');
             // send welcome msg
             String welcomeMsg = "Welcome! Please enter your name: ";
             outToClient.writeBytes(welcomeMsg + "\r\n");
@@ -45,7 +49,7 @@ class WebServer {
             System.out.println(clientName + " joined the chat!");
 
             // Send hello msg to the newly connected client
-            String helloMsg = "Hello " + clientName + "! to the NP chatroom! If you would like to leave, type {quit}";
+            String helloMsg = "Hello " + clientName + "! to the ConnectFour lobby! If you would like to leave, type {quit}";
             outToClient.writeBytes(helloMsg + "\r\n");
 
             // broadcast
@@ -67,7 +71,20 @@ class WebServer {
                     else if (clientMessage.equals("{quit}")) quitCommand(clientName);
 
                     else if (clientMessage.contains("{place}") && full) {
-                        if (players.containsKey(clientName)) placeCommand(clientName, clientMessage); 
+                        if (players.containsKey(clientName)) {
+                            if(playersArr.get(clientName) == 'R' && !redWent) {
+                                int col = placeCommand(clientName, clientMessage); 
+                                board.chooseAndDrop(playersArr.get(clientName), col);
+                                redWent = true;                                
+                            }
+                            else if(playersArr.get(clientName) == 'Y' && redWent) {
+                                int col = placeCommand(clientName, clientMessage); 
+                                board.chooseAndDrop(playersArr.get( clientName ), col);
+                                redWent = false;
+                            }    
+                            else players.get(clientName).writeBytes("You've already went, you must wait your turn.");
+                            
+                        }
                         else clients.get(clientName).writeBytes("You're not a player. Only players can make moves.");
                     }
 
@@ -90,7 +107,8 @@ class WebServer {
                 
                 //add to player
                 players.put(clientName, spectators.get(clientName));
-
+                if(players.isEmpty()) playersArr.put( clientName, 'R' );
+                else playersArr.put( clientName, 'Y' );
                 //remove from spectator
                 spectators.remove(clientName);
                 
@@ -112,9 +130,10 @@ class WebServer {
             for (String name : clients.keySet()) clients.get(name).writeBytes(clientName + " left the chat!\r\n");
         }
 
-        private void placeCommand(String clientName, String clientMessage) {
+        private int placeCommand(String clientName, String clientMessage) {
             int col = Integer.parseInt(clientMessage.split(" ")[1]); // this is the column to place the marker in
             System.out.println(clientName + " has placed a marker on column " + col);
+            return col;
         }
 
         public static void main(String argv[]) throws Exception {
@@ -137,4 +156,30 @@ class WebServer {
             }
         }
     }
+    
+//    public static void main(String args[])
+//    {
+//    
+//    //we assemble all the pieces of the puzzle for 
+//    // building our Con nect Four Game
+//    try (Scanner input = new Scanner(System.in)) {
+//      // we create the ConnectFour instance
+//      ConnectFour board = new ConnectFour(7, 6, 'R', 'Y');
+//    
+//      // we explain users how to enter their choices
+//      System.out.println("Use 0-" + (board.getWidth() - 1) + " to choose a column");
+//      // we display initial board
+//      System.out.println(board.showBoard());
+//    
+//      // we iterate until max nb moves be reached
+//      // simple trick to change player turn at each iteration
+//      int moves = board.getMoves();
+//      for (int player = 0; moves-- > 0; player = 1 - player) {
+//        ConnectFour.playerTurn(board, player, 3);
+//      }
+//      System.out.println("Game over. No winner. Try again!");
+//    }
+  
+    
+    
 }
